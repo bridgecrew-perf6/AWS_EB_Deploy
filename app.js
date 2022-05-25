0060c8244584f03e8ef4ca07a095a2d9dc880063
dipakparmar13 <102644680+dipakparmar13@users.noreply.github.com>
2022-05-25 14:28:15 +0530

    version 3 added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("VERSION 2.0");
+  res.send("VERSION 3.0");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
